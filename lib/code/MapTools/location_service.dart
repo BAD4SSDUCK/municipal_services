90389288cfa1bb1ce914513fb737b1cadcea2618
BAD4SSDUCK <daksha.krishnan@gmail.com>
2023-09-15 16:29:21 +0200

    Master Commit: Update entire app to run on web browsers. Mapping on browser to fix

--- lib/code/MapTools/location_service.dart
+++ lib/code/MapTools/location_service.dart
@@ -6,7 +6,7 @@ Future<http.Response> getLocationData(String text) async {
 
   ///Using the same uri url from a tutorial video, will need to check if that api works or is outdated
   response = await http.get(
-    Uri.parse("https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$text&key=AIzaSyB3p4M0JwkbBauV_5_dIHxWNpk8PSqmmU0"),
+    Uri.parse("https://maps.googleapis.com/maps/api/place/autocomplete/json?input=$text&key=AIzaSyCsOGfD-agV8u68pCfeCManNNoSs4csIbY"),
     headers: {"Content-Type": "application/json"},);
 
   print(jsonDecode(response.body));
